8e75a6a2daafd90f8cac6edbffa3206af7ea113a
thkiaAdiba <athkiaadiba@gmail.com>
2024-04-29 22:49:58 +0600

    added react typewriter

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -3,6 +3,7 @@ import Country from "../Country/Country";
 import { Typewriter } from 'react-simple-typewriter'
 
 
+
 const Countries = () => {
     const [countriesData, setCountriesData] = useState([]);
     // console.log(countriesData)
@@ -17,7 +18,17 @@ const Countries = () => {
     return (
         <div className="mx-[5%] mt-28 lg:mt-44 mb-20">
             <p className="tracking-[10px] text-[#2095AE] font-barlow mb-6 text-lg">CHOOSE YOUR PLACE</p>
-            <h2 className="text-6xl font-bold mb-10 leading-normal"><span className="text-[#0f2454] dark:text-white">DISCOVER THE</span> <span className="text-[#2095AE]">WORLD</span> <br /><span className="text-[#0f2454] dark:text-white">WITH US</span></h2>
+            <h2 className="text-6xl font-bold mb-10 leading-normal"><span className="text-[#0f2454] dark:text-white">
+                <Typewriter
+                    words={['DISCOVER THE', 'DISCOVER THE', 'DISCOVER THE', 'DISCOVER THE']}
+                    loop={5}
+                    typeSpeed={70}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+
+                />
+
+            </span> <span className="text-[#2095AE]">WORLD</span> <br /><span className="text-[#0f2454] dark:text-white">WITH US</span></h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
                 {
                     countriesData.map(country => <Country key={country._id} country={country}></Country>)
